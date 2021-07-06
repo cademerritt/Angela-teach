ef8d3d1a051da17af752ef78b3bea8902ab93eb8
cademerritt <39072260+cademerritt@users.noreply.github.com>
2021-07-05 21:04:09 -0400

    Delete intro-to-node directory

--- intro-to-node/index.js
+++
@@ -1,7 +0,0 @@
-var superheroes = require("superheroes");
-const supervillains = require('supervillains');
-
-var mySuperheroname=superheroes.random();
-var mySupervillainname=supervillains.random();
-
-console.log(mySuperheroname + " VS " + mySupervillainname);
